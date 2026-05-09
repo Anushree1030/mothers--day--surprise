62e5d31dbc50a2f25ca49e70fecb09644e432b5a
ANUSHREE <shreeriddhi154@gmail.com>
2026-05-10 01:39:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openMain(){
+
+  document.getElementById("intro").classList.add("hidden");
+
+  document.getElementById("main").classList.remove("hidden");
+}
+
+function showPhoto(){
+
+  document.getElementById("photoSection").classList.remove("hidden");
+}
